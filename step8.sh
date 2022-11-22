a3baa37115dcd0d503d0ad18fb156eaa5db425dd
caraortizmah <caraortizmah@gmail.com>
2022-11-22 18:30:53 +0100

    enabling to extract features by an specific region of excited states

--- step8.sh
+++ step8.sh
@@ -3,9 +3,20 @@
 #Creating core/virt MO matrix as a function of the total transition state ocurrences
 
 out2_file1="$1" # exc_states_transitions.out
+arg="$2"
 
-#cleaning transition states file
-sed -ne '/STATE   1 /,$ p' $out2_file1 > exc_states.tmp
+#cleaning transition states file by a defined range of excited states
+if (($arg=="none")); then
+        sed -ne '/STATE   1 /,$ p' $out2_file1 > exc_states.tmp
+else
+	ex_ini="$(echo $arg | cut -d'-' -f1)"
+	ex_fin="$(echo $arg | cut -d'-' -f2)"
+	sed -ne "/$(echo STATE $ex_ini | awk '{printf("%s%4d ",$1,$2)}')/,/$(echo STATE $ex_fin | awk '{printf("%s%4d",$1,$2+1)}')/p" exc_states_transitions.out  > exc_states.tmp
+fi
+
+#sed -ne "/STATE   1 /,/$(echo $var1 $var3 | awk '{printf("%s%4d",$1,$2+1)}')/p" exc_states_transitions.out  > exc_states.tmp
+
+#sed -ne '/STATE   1 /,$ p' $out2_file1 > exc_states.tmp
 #extracting just rows containing core to virtual MO transitions
 awk 'NF { if ( $1 != "STATE" && $1 != "Calculating"){ print $0 } }' exc_states.tmp > exc_states.tmp2
 
@@ -59,7 +70,8 @@ mv corevirtMO_matrix_ts_probability.tmp corevirtMO_matrix_ts_probability.out
 sed -r 's/\s+/,/g' corevirtMO_matrix.out > corevirtMO_matrix.csv
 sed -r 's/\s+/,/g' corevirtMO_matrix_ts_probability.out > corevirtMO_matrix_ts_probability.csv
 
-rm exc_states.tmp exc_states.tmp2
-
+rm exc_states.tmp2
+head -n -1 exc_states.tmp > test2
+mv test2 exc_states.tmp
 #four files as outputs from this script (corevirtMO_matrix.out, corevirtMO_matrix_ts_probability.out,
 # corevirtMO_matrix.csv, corevirtMO_matrix_ts_probability.csv)
