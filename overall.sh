8104d17f48176fbc34afa50dffa1036fc9647521
caraortizmah <caraortizmah@gmail.com>
2023-09-18 13:15:04 +0200

    updating overall.sh to receive the work diretory (absolute one) as an external variable

--- overall.sh
+++ overall.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-#list="2.5 2.6 2.8 2.9 3.0 4.0 4.5 5.0 6.0 6.5 7.0 8.0 8.5 9.5 10.0 10.5"
 
-for ii in `ls ../outs/*.out`
+workdir="$1"
+
+for ii in `ls ${workdir}/*.out`
 do
-	job="$(echo "$ii" | cut -d'/' -f3)"
+	job="$(echo "$ii" | awk -F '[/]' '{print $(NF)}')"
 	cp $ii .
 	./manager.sh 0 22 23 46 7 24 $job
 done
