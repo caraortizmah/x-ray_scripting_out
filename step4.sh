3848eabae54379d724dce516bb7265567793371b
caraortizmah <caraortizmah@gmail.com>
2023-10-06 16:41:14 +0200

    adding last corrections

--- step4.sh
+++ step4.sh
@@ -7,24 +7,27 @@ arg_exc="$4" #excited states range
 
 # cleaning transition states file by a defined range of excited states
 if (($arg_exc=="none")); then
+	
+	# copying all the excited states (default: none)
         sed -ne '/STATE   1 /,$ p' $out2_file1 > exc_states.tmp
 else
-	ex_ini="$(echo $arg_exc | cut -d'-' -f1)"
-	ex_fin="$(echo $arg_exc | cut -d'-' -f2)"
+	# copying a range of excited states specified in $arg_exc
+	ex_ini="$(echo $arg_exc | cut -d'-' -f1)" # initial excited state
+	ex_fin="$(echo $arg_exc | cut -d'-' -f2)" # final excited state
 	sed -ne "/$(echo STATE $ex_ini | awk '{printf("%s%4d ",$1,$2)}')/,/$(echo STATE $ex_fin | awk '{printf("%s%4d",$1,$2+1)}')/p" $out2_file1 > exc_states.tmp
 fi
 
 state_line="$(grep -n "STATE " exc_states.tmp | cut -d':' -f1)" #getting position lines having info
 
-#getting the excited state number involved in each field having transitions information
-#same excited state number can be involved in several transitions from different fields,
-# so repeated information is collected
 
+# getting the excited state number involved in each field having transitions information
+# same excited state number can be involved in several transitions from different fields,
+# so repeated information is collected
 atm_lst="$(awk '$2=="C"{ printf "%d\n", $1 }' $out1_file3)" #getting target atoms list
 #trans_line="$(grep -n " C s" $out_file3 | cut -d':' -f1)" #getting position lines having info
 
-#getting the unique elements (MO numbers)
-uniq_MO="$(cat $out2_file3 | sort -u -n | uniq)"
+# creating a list of unique MOs numerically ordered
+uniq_MO="$(cat $out2_file3 | sort -nu | uniq)"
 
 rm -rf trans_st.out virt_MO.tmp trans_st.tmp trans_st_2.tmp trans_st_2_1.tmp virt_MO.tmp
 
@@ -39,7 +42,7 @@ do
 	    #if there is not a match looking for $jj (core MO) skip that excited state
 	    if (( $(wc -l trans_st_2_1.tmp | cut -d' ' -f1) > 0 )); then
 
-		    sed -n "${ii}p" exc_states.tmp >> trans_st.tmp #copying head of that state
+                    sed -n "${ii}p" exc_states.tmp >> trans_st.tmp #copying head of that state
                     awk '{printf "%s\n", $0}' trans_st_2_1.tmp >> trans_st.tmp
 	    fi
 	    sed -n "/  ${jj}->/p" trans_st_2.tmp | cut -d'>' -f2 | cut -d':' -f1 | cut -d' ' -f1 >> virt_MO.tmp
