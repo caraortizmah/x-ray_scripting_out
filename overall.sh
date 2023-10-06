9635ca6f0e2309bec42b8153396a8ec73a75b4bd
caraortizmah <caraortizmah@gmail.com>
2023-10-06 18:12:08 +0200

    updating the command line used to run the manager.sh

--- overall.sh
+++ overall.sh
@@ -9,5 +9,5 @@ for ii in `ls ${workdir}/*.out`
 do
 	job="$(echo "$ii" | awk -F '[/]' '{print $(NF)}')"
 	cp $ii .
-	./manager.sh 0 22 23 46 7 24 $job
+	./manager.sh 0 22 23 46 7 24 0 $job 1-17
 done
