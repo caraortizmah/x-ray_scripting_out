a4c6d2d1993a96456128db63d79668bab91365b4
caraortizmah <caraortizmah@gmail.com>
2023-11-09 17:08:12 +0100

    Updating overall according to the last changes and new external commands

--- overall.sh
+++ overall.sh
@@ -9,5 +9,5 @@ for ii in `ls ${workdir}/*.out`
 do
 	job="$(echo "$ii" | awk -F '[/]' '{print $(NF)}')"
 	cp $ii .
-	./manager.sh 0 22 23 46 7 24 0 $job 1-17
+	./manager.sh 0 46 0 46 7 24 0 $job 1-26 1
 done
