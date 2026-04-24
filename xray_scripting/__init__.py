fd1eb8ea13a87d25001b990d1f669e86cf1c87a8
caraortizmah <caraortizmah@gmail.com>
2026-04-24 16:16:29 +0200

    add mypackage style like a unified API

---
+++ xray_scripting/__init__.py
@@ -0,0 +1,15 @@
+"""
+X-ray Absorption Spectroscopy Data Processing Pipeline
+
+A package for processing (data parser) X-ray Absorption Spectroscopy (XAS) 
+output data from ORCA (4.0 and 5.0 versions) quantum chemistry software.
+"""
+
+__version__ = "3.0.0"
+__author__ = "Carlos Ortiz-Mahecha"
+
+from .config import ConfigManager
+from .logger import setup_logger
+from .validator import ConfigValidator
+
+__all__ = ["ConfigManager", "setup_logger", "ConfigValidator"]
